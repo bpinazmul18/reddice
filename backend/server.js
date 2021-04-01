cfacc9548f90db8ba0b0509c66a31dbd455639c2
Md Nazmul Haque <48866588+bpinazmul18@users.noreply.github.com>
2021-04-01 13:20:47 +0600

    usersingup server side validation

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,48 @@ const PORT = process.env.PORT || 8080;
 const dbURL = "mongodb://localhost/reddicedb";
 app.use(bodyParser.json());
 
+function validate(data) {
+  let errors = {};
+  if (data.username === "") errors.username = "Can't be empty";
+  if (data.email === "") errors.email = "Can't be empty";
+  if (data.password === "") errors.password = "Can't be empty";
+  if (data.repeatPassword === "") errors.repeatPassword = "Can't be empty";
+  if (data.timezone === "") errors.timezone = "Can't be empty";
+  const isValid = Object.keys(errors).length === 0;
+
+  return { errors, isValid };
+}
+
 mongodb.MongoClient.connect(dbURL, (err, db) => {
   app.post("/api/users", (req, res) => {
-    console.log(req.body);
+    const { errors, isValid } = validate(req.body);
+    console.log(errors);
+    console.log(isValid);
+
+    if (isValid) {
+      const { username, email, password, repeatPassword, timezone } = req.body;
+
+      console.log("username", username);
+      console.log("email", email);
+      console.log("password", password);
+      console.log("repeatPassword", repeatPassword);
+      console.log("timezone", timezone);
+
+      db.collection("users").insert(
+        { username, email, password, repeatPassword, timezone },
+        (err, result) => {
+          if (err) {
+            res
+              .status(500)
+              .json({ errors: { global: "Something went wrong!" } });
+          } else {
+            res.json({ user: result.ops[0] });
+          }
+        }
+      );
+    } else {
+      res.status(400).json({ errors });
+    }
   });
 
   app.listen(PORT, (err) => {
